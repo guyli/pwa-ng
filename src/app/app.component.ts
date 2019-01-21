dd1a46e3638e1458d9778dfb9a25571b27111008
Guy <guyko18@gmail.com>
2019-01-20 20:07:12 -0500

    Running the app and auditing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ApiService } from './api.service';
 import { Item } from './api.service';
 
@@ -7,6 +7,23 @@ import { Item } from './api.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pwa-ng';
+  items: Array<Item>;
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.apiService.fetch().subscribe((data: Array<Item>) => {
+      console.log('%c Data fetched from api service:', 'color: salmon; font-size: 1rem; font-weight: bold');
+      console.log(data);
+      this.items = data;
+    }, (err) => {
+      console.error(err);
+    });
+  }
 }
